a399ca4780088ad911e2e1125f67cd6a23934714
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2026-05-17 11:11:22 -0700

    removed [0] broadcast in xover inputs. This was preventing anything (unless udnerlying very fortuante circumstances) from making it through offline.

--- moi/Input.py
+++ moi/Input.py
@@ -283,7 +283,7 @@ class Input:
                                np.isnan(self.obs_dict[reach]['S']) | \
                                np.isnan(self.obs_dict[reach]['dA'])| \
                                (self.obs_dict[reach]['reach_q'] > 1) | \
-                               (self.obs_dict[reach]['xovr_cal_q'] > 1)[0] )
+                               (self.obs_dict[reach]['xovr_cal_q'] > 1) )
 
             self.obs_dict[reach]['h'] = np.delete(self.obs_dict[reach]['h'], iDelete, 0)
             self.obs_dict[reach]['w'] = np.delete(self.obs_dict[reach]['w'], iDelete, 0)
