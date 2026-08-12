ad68b29672ce25310291312bcec7d6071c8acf33
mikedurand <durand.8@osu.edu>
2026-08-12 13:28:29 -0400

    Read Corridors datafiles.

--- moi/Corridors.py
+++ moi/Corridors.py
@@ -2,6 +2,7 @@ import csv
 from pathlib import Path
 import warnings
 import sys
+import pandas as pd
 
 class Corridors:
     """Extracts and formats CORRIDORS data from CSV files."""
@@ -28,38 +29,31 @@ class Corridors:
         """
         if self.verbose:
             print(f"  -> Scanning for CORRIDORS CSV files in: {self.corridors_dir}")
-            
+             
+        # grab csv file, and exclude the translation file
         csv_files = list(self.corridors_dir.glob("*.csv"))
+        csv_files=[f for f in csv_files if f.name != 'SWORD_v16_v17_translation_reach.csv']
 
-        print('found these files: ',csv_files)
-        sys.exit()
-        
         if not csv_files:
             warnings.warn(f"No CSV files found in {self.corridors_dir}.")
             return self.corridors_dict
 
-        '''
+        corridors_dfs=[]
         for csv_file in csv_files:
             if self.verbose:
                 print(f"  -> Processing {csv_file.name}...")
-                
-            # Basic I/O setup - you can build your custom logic in this block
             try:
-                with csv_file.open('r', newline='', encoding='utf-8-sig') as stream:
-                    reader = csv.DictReader(stream)
-                    
-                    for row in reader:
-                        # TODO: Fiddle with operations here!
-                        # Example mimicking sos_dict insertion:
-                        # reach_id = row.get('reach_id')
-                        # if reach_id in input_obj.basin_dict['reach_ids_all']:
-                        #     self.corridors_dict[reach_id] = {
-                        #         'my_custom_value': row.get('value', 0)
-                        #     }
-                        pass
+                corridors_dfs.append(pd.read_csv(csv_file))
                         
             except Exception as e:
                 print(f"  -> Error reading {csv_file.name}: {e}")
-        '''
+        corridors_df=pd.concat(corridors_dfs,ignore_index=True)
+
+        print(corridors_df)
+
+        
+        print('end of corridors setup. quitting')
+        sys.exit(1)
+        
 
         return self.corridors_dict
