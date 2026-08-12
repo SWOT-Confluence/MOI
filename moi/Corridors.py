eea41658f78bfe1dc812422f5629952e8299c1ce
mikedurand <durand.8@osu.edu>
2026-08-12 13:59:09 -0400

    Add SWORD17 reachids to Corridors dataframe.

--- moi/Corridors.py
+++ moi/Corridors.py
@@ -27,16 +27,22 @@ class Corridors:
         dict
             A dictionary structured similarly to sos_dict.
         """
+
         if self.verbose:
             print(f"  -> Scanning for CORRIDORS CSV files in: {self.corridors_dir}")
              
         # grab csv file, and exclude the translation file
         csv_files = list(self.corridors_dir.glob("*.csv"))
-        csv_files=[f for f in csv_files if f.name != 'SWORD_v16_v17_translation_reach.csv']
+        trans_fname='SWORD_v16_v17_translation_reach.csv'
+        csv_files=[f for f in csv_files if f.name != trans_fname] 
+        for f in csv_files:
+            if f.name=trans_name:
+                trans_file=f
 
+        # read corridors data
         if not csv_files:
             warnings.warn(f"No CSV files found in {self.corridors_dir}.")
-            return self.corridors_dict
+            return None
 
         corridors_dfs=[]
         for csv_file in csv_files:
@@ -47,13 +53,34 @@ class Corridors:
                         
             except Exception as e:
                 print(f"  -> Error reading {csv_file.name}: {e}")
-        corridors_df=pd.concat(corridors_dfs,ignore_index=True)
 
-        print(corridors_df)
+        if corridors_dfs:
+            self.corridors_df=pd.concat(corridors_dfs,ignore_index=True)
+        else:
+            return None
 
+        # add sword17 to dataframe
+        self.add_sword_17_ids(trans_file) 
+
+        print(self.corridors_df['reach_id_17'])
         
         print('end of corridors setup. quitting')
         sys.exit(1)
         
 
         return self.corridors_dict
+
+    def add_sword_17_ids(self,trans_file):
+        self.corridors_df['reach_id_17']=None
+        try:
+            transdf=pd.read_csv(trans_file)
+        except Exception as e:
+            print(f"  -> Error reading SWORD16-17 translation file {trans_file.name}: {e}")
+            return
+
+        rids_16=list(set(list(self.corridors_df['Reach_ID'])))
+        for rid in rids_16:
+            rid17=int(transdf[transdf['v16_reach_id']==rid]['v17_reach_id'].iloc[0])
+            self.corridors_df.loc[self.corridors_df['Reach_ID']==rid,'reach_id_17']=rid17
+
+
